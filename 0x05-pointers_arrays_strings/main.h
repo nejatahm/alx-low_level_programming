934cb41348e0bb75cc5d495d25ae1820e88383cf
nejatahm <nejah2005@gmail.com>
2022-03-21 11:30:18 -0700

    my array and pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#define FILE_MAIN
-
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,6 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int_putchar(char c);
-
-#endif
