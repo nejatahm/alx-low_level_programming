1c6a3fa658e0c03a5b2a3cb5b0fe4be09f2b74cb
nejatahm <nejah2005@gmail.com>
2022-03-30 12:44:37 -0700

    my recurrsion commits

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,18 +1,4 @@
-#include "main.h"
-
-/**
- * _strlen - return length of string
- * @str: string to check
- *
- * Return: length of str
- */
-int _strlen(char *str)
-{
-	if (*str == '\0')
-		return (0);
-	else
-		return (1 + _strlen(str + 1));
-}
+i#include "holberton.h"
 
 /**
  * check_palindrome - checks to see if a string is a palindrome
