c7801a41202cfe5eb13d48e694fece34ea6f0eb3
nejatahm <nejah2005@gmail.com>
2022-03-30 12:07:58 -0700

    my recurrsion commits

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- * Sqrt_check - checks for the square root of c
- * @g: guess at sqrt
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
  * @c: number to find sqrt of
  *
  * Return: -1 or sqrt of c
  */
 int sqrt_check(int g, int c)
-{	
+{
 	if (g * g == c)
-		return(g);
+		return (g);
 	if (g * g > c)
 		return (-1);
 	return (sqrt_check(g + 1, c));
 }
-
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
