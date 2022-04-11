77a492f7d53181b48a022668e47bee493815982b
nejatahm <nejah2005@gmail.com>
2022-04-11 13:05:04 -0700

    print name of a file commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,13 @@
- #nclude <stdio.h>
+#include <stdio.h>
 
- /**
-  * main - prints the name of the file it was compiled from
-  *
-  * Return: Everything worked ok
-  */
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
 
- int main(void)
- {	
-	 printf("%s\n" , __BASE_FILE__);
-	 return (0);
-
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
 }
